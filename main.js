66aec45fb532c70dbc2ae42016da04674b517188
Alihan <alihan@MacBook-Berk.local>
2022-06-07 11:39:07 +0600

    added post request

--- main.js
+++ main.js
@@ -11,3 +11,24 @@ GET - получение данных
 Команды для запроса json-server
 json-server -w db.json -p 8000
  */
+/*
+CRUD - Create(POST-request) Read(GET-request) Update(PUT/PATCH-request) Delete(DELETE-request)
+ */
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById("inp-add");
+let btnAdd = document.getElementById("btn-add");
+// console.log(inpAdd, btnAdd);
+btnAdd.addEventListener("click", function () {
+  let newTodo = {
+    todo: inpAdd.value,
+  };
+  // console.log(newTodo);
+  fetch(API, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+});
